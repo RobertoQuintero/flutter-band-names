8f1dd144bc4ee9e8b8e3f1b54db57873502689fd
Roberto Quintero <langdonhp435@gmail.com>
2021-06-25 13:37:27 -0500

    Fin sección 2

---
+++ lib/models/band_model.dart
@@ -0,0 +1,10 @@
+class BandModel {
+  String id;
+  String name;
+  int votes;
+
+  BandModel({required this.id, required this.name, this.votes = 0});
+
+  factory BandModel.fromMap(Map<String, dynamic> obj) =>
+      BandModel(id: obj['id'], name: obj['name'], votes: obj['votes']);
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,135 @@
+import 'dart:io';
+
+import 'package:band_names/models/band_model.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<BandModel> bands = [
+    BandModel(id: '1', name: 'Metallica', votes: 5),
+    BandModel(id: '2', name: 'Queen', votes: 1),
+    BandModel(id: '3', name: 'Heroes del silencio', votes: 2),
+    BandModel(id: '4', name: 'Bon Jovi', votes: 5),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        centerTitle: true,
+        title: Text(
+          'BandNames',
+          style: TextStyle(color: Colors.black87),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+          itemCount: bands.length,
+          itemBuilder: (context, index) => BandTile(band: bands[index])),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        elevation: 1,
+        onPressed: addNewBand,
+      ),
+    );
+  }
+
+  void addNewBand() {
+    final textController = TextEditingController();
+    if (!Platform.isAndroid) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('New Band Name'),
+              content: TextField(
+                controller: textController,
+              ),
+              actions: [
+                MaterialButton(
+                    child: Text('Add'),
+                    elevation: 5,
+                    onPressed: () => addBandToList(textController.text))
+              ],
+            );
+          });
+    } else {
+      showCupertinoDialog(
+          context: context,
+          builder: (_) {
+            return CupertinoAlertDialog(
+              title: Text('New band name'),
+              content: CupertinoTextField(
+                controller: textController,
+              ),
+              actions: [
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: Text('Add'),
+                  onPressed: () => addBandToList(textController.text),
+                ),
+                CupertinoDialogAction(
+                  isDestructiveAction: true,
+                  child: Text('Dismiss'),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ],
+            );
+          });
+    }
+  }
+
+  void addBandToList(String name) {
+    if (name.length > 1) {
+      print('');
+      this.bands.add(BandModel(id: DateTime.now().toString(), name: name));
+      setState(() {});
+    }
+    Navigator.pop(context);
+  }
+}
+
+class BandTile extends StatelessWidget {
+  const BandTile({
+    required this.band,
+  });
+
+  final BandModel band;
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: Key(band.id),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {},
+      background: Container(
+        padding: EdgeInsets.only(left: 10),
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment.centerLeft,
+          child: Text('Delete Band', style: TextStyle(color: Colors.white)),
+        ),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(
+            band.name.substring(0, 2),
+          ),
+          backgroundColor: Colors.blue[100],
+        ),
+        title: Text(band.name),
+        trailing: Text(
+          '${band.votes}',
+          style: TextStyle(fontSize: 20),
+        ),
+        onTap: () {},
+      ),
+    );
+  }
+}
